9d4acce5840a8ce1c6ca30a4b04966a058afceca
3dw1nM0535 <workockmoses@gmail.com>
2020-09-19 18:15:22 +0300

    chore:test cases

--- tests/test_seasons.py
+++ tests/test_seasons.py
@@ -11,7 +11,41 @@ def season_contract(Season, accounts):
     yield Season.deploy(frmregistry_contract.address, {'from': accounts[0]})
 
 def test_initial_state(season_contract):
+
     # Assertions
     assert season_contract.currentSeason(token_id) == 0
     assert season_contract.completeSeasons() == 0
+
+def test_invalid_token_season(season_contract):
+
+    # Error assertions
+    with brownie.reverts():
+        season_contract.getSeason(4992)
+
+def test_get_token_season(season_contract):
+
+    # Assertions
+    assert season_contract.getSeason(token_id) == 'Dormant'
+
+def test_get_invalid_farm_season_data(season_contract):
+
+    # Error assertions
+    with brownie.reverts():
+        season_contract.querySeasonData(3892, 1)
+
+def test_get_index_farm_season_data(season_contract):
+
+    # Error assertions
+    with brownie.reverts():
+        season_contract.querySeasonData(token_id, 1)
+
+def test_query_season_data(season_contract):
+
+    # Assertions
+    total_complete_season = season_contract.currentSeason(token_id)
+    season_data = list()
+    for i in range(1, total_complete_season+1):
+        season_data.append(season_contract.querySeasonData(token_id, i))
+
+    assert len(season_data) == 0
 
