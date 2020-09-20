de8c5697c3cbc8f8f8d113a40c553cf338aa39bc
3dw1nM0535 <workockmoses@gmail.com>
2020-09-20 17:04:46 +0300

    chore:test cases

--- tests/test_seasons.py
+++ tests/test_seasons.py
@@ -1,11 +1,10 @@
 import pytest
 import brownie
-from brownie import FRMRegistry
 
-token_id = 4833475
+token_id = 4863475
 
 @pytest.fixture
-def season_contract(Season, accounts):
+def season_contract(Season, FRMRegistry, accounts):
     frmregistry_contract = FRMRegistry.deploy({'from': accounts[0]})
     frmregistry_contract.tokenizeLand('Arunga Vineyard', '294.32ha', '36.389223', '-1.282883', 'QmUfideC1r5JhMVwgd8vjC7DtVnXw3QGfCSQA7fUVHK789', 'loam soil', token_id, {'from': accounts[0]})
     yield Season.deploy(frmregistry_contract.address, {'from': accounts[0]})
@@ -15,6 +14,7 @@ def test_initial_state(season_contract):
     # Assertions
     assert season_contract.currentSeason(token_id) == 0
     assert season_contract.completeSeasons() == 0
+    assert season_contract.getSeason(token_id) == 'Dormant'
 
 def test_get_tokenized_farm_current_season(season_contract):
 
@@ -42,6 +42,25 @@ def test_get_token_season(season_contract):
 
     # Assertions
     assert season_contract.getSeason(token_id) == 'Dormant'
+
+def test_farm_season_opening(season_contract):
+    season_contract.openSeason(token_id)
+
+    # Assertions
+    assert season_contract.currentSeason(token_id) == 1
+    assert season_contract.getSeason(token_id) == 'Preparation'
+
+def test_unrestricted_farm_season_opening(season_contract, accounts):
+
+    # Error assertions
+    with brownie.reverts('dev: only owner can transition farm state'):
+        season_contract.openSeason(token_id, {'from': accounts[1]})
+
+def test_unrestricted_season_closure(season_contract):
+
+    # Error assertions
+    with brownie.reverts('dev: is not harvesting'):
+        season_contract.closeSeason(token_id)
 
 def test_get_invalid_farm_season_data(season_contract):
 
